8913be378e458ee211accc801c59f34e1d6de825
Brandon Donato-Long <wartornhero@gmail.com>
2017-04-29 21:59:28 -0700

    check in for the end of the night. Be back tomorrow

--- CoordinateConverter.js
+++ CoordinateConverter.js
@@ -2,6 +2,48 @@
  * Created by BNB on 4/29/2017.
  */
 var fs = require('fs');
-modules.export = coordinateConverter = function(latFile, lonFile){
+module.exports = coordinateConverter = function(latFile, lonFile){
+    this.latitudes;
+    this.longitudes;
+
+    var latData = fs.readFileSync(latFile, 'utf8');
+    this.latitudes = latData.split(',');
+    for (var index = 0; index < this.latitudes.length; index++){
+        this.latitudes[index] = parseFloat(this.latitudes[index]);
+    }
+    // console.log(this.latitudes);
+    var lonData = fs.readFileSync(lonFile, 'utf8');
+    this.longitudes = lonData.split(',');
+    for (var index = 0; index < this.longitudes.length; index++){
+        this.longitudes[index] = parseFloat(this.longitudes[index]);
+    }
+    // console.log(this.longitudes);
+};
+
+coordinateConverter.prototype.getCoordinateIndexes = function(lat, long){
+    var returnCoordinates = {lat:0, lon:0};
+    console.log(lat, long);
+    // long = Math.abs(long);
+    console.log(this.longitudes[27], this.longitudes[28]);
+    //do this to convert to 360 degrees.
+    if (long < 0){
+        long = long + 360;
+    }
+    for (var index = 0; index < this.latitudes.length-1; index++ ){
+        if (lat > this.latitudes[index] && lat < this.latitudes[index+1]){
+            console.log("found a good latitude", index, this.latitudes[index], this.latitudes[index+1]);
+            returnCoordinates.lat = index+1;
+            break;
+        }
+    }
+    for (var index = 0; index < this.longitudes.length-1; index++ ){
+        if (long > this.longitudes[index] && long < this.longitudes[index+1]){
+            console.log("found a good longitude", index, this.longitudes[index], this.longitudes[index+1]);
+            returnCoordinates.lon = index+1;
+            break;
+        }
+    }
+    return returnCoordinates;
+
 
 }
--- main.js
+++ main.js
@@ -18,12 +18,39 @@
 
 var util = require('util');
 var http = require('http');
-var formatUrl = "http://data1.gfdl.noaa.gov:9192/opendap/gfdl_cm2_1/CM2.1U-H2_SresA1FI_Z1/pp/atmos/ts/annual/ts_A0.2001-2100.nc.ascii?ts[%s][%d][%d]";
+var formatUrl = "http://data1.gfdl.noaa.gov:9192/opendap/gfdl_cm2_1/CM2.1U-H2_SresA1FI_Z1/pp/atmos/ts/annual/ts_A0.2001-2100.nc.ascii?ts[%s][%s][%s]";
 
-var CoordinateConverter = require('CoordinateConverter');
+//regex to get the time
+var timeRegex = /time=[0-9.]*/;
+var latRegex = /lat=[0-9.]*/;
+
+
+var CoordinateConverter = require('./CoordinateConverter');
 var cc = new coordinateConverter('./data/lat.csv', './data/lon.csv');
+//west
+var coordinates2 = cc.getCoordinateIndexes(39, -119.5);
+//east
+var coordinates1 = cc.getCoordinateIndexes(45, -118.0);
 
-http.get(util.format(formatUrl, '0:1:99', 39, 143), function (res) {
+var latString = coordinates1.lat.toString();
+var lonString = coordinates1.lon.toString();
+
+var latDiff = (coordinates1.lat - coordinates2.lat);
+var lonDiff = (coordinates1.lon - coordinates2.lon);
+if (latDiff > 0){
+    latString = util.format("%d:%d",coordinates1.lat, coordinates1.lat + latDiff);
+} else if (latDiff < 0){
+    latString = util.format("%d:%d",coordinates1.lat, coordinates1.lat - lonDiff);
+}
+
+if (lonDiff > 0){
+    lonString = util.format("%d:%d",coordinates1.lon, coordinates1.lon + lonDiff);
+} else if (lonDiff < 0){
+    lonString = util.format("%d:%d",coordinates1.lon, coordinates1.lon - lonDiff);
+}
+
+
+http.get(util.format(formatUrl, '1', latString, lonString), function (res) {
     res.setEncoding('utf8');
     var rawData = '';
     res.on('data', function(chunk) { rawData += chunk; });
@@ -31,17 +58,52 @@ http.get(util.format(formatUrl, '0:1:99', 39, 143), function (res) {
         try {
             var lines = rawData.split("\n");
             var dataLines = [];
+            var longitudes = [];
+            var latArray = [];
             lines.forEach(function(line){
-                if (line.indexOf("ts.ts[") > -1){
-                    line = line.substring(line.indexOf('['));
-                    var dataSet = line.split(', ');
-                    dataSet[0] = dataSet[0].substring(dataSet[0].indexOf('=')+1, dataSet[0].indexOf(']'));
-                    //At this point the datalines is now [Time]; 0 = [degreesInKelvin];
-                    dataLines.push(dataSet);
+                if (line.indexOf("ts.lon") > -1){
+                    //use this to get the longitude indexes
+                    longitudes = line.split(", ");
+                    longitudes = longitudes.splice(1);
+                    for (var index = 0; index < longitudes.length; index++){
+                        longitudes[index] = parseFloat(longitudes[index]);
+                    }
                 }
-            });
+                //parse out the data
+                if (line.indexOf("ts.ts[") > -1){
+                    var latString = latRegex.exec(line)[0];
+                    var latValue = parseFloat(latString.substring(latString.indexOf('=')+1));
+                    if (latArray.indexOf(latValue) < 0){
+                        latArray.push(latValue);
+                    }
+                    var temps = line.split(', ');
+                    temps = temps.splice(1);
+                    for (var index = 0; index < longitudes.length; index++) {
+                        var dataEntry = {lat:latValue, lon: longitudes[index], temp:(parseFloat(temps[index])-273.15).toPrecision(4)};
+                        dataLines.push(dataEntry)
+                    }
+                }
 
-            console.log(dataLines, dataLines.length);
+            });
+            var sendData = {};
+            sendData.lons = longitudes;
+            sendData.lats = latArray;
+            sendData.data = [];
+            for (var latIndex = 0; latIndex < latArray.length; latIndex++){
+                sendData.data[latIndex] = [];
+                for(var lonIndex = 0; lonIndex < longitudes.length; lonIndex++){
+                    sendData.data[latIndex][lonIndex] = 0;
+                }
+            }
+
+            for (var index = 0; index < dataLines.length; index++){
+                var latIndex = sendData.lats.indexOf(dataLines[index].lat);
+                var lonIndex = sendData.lons.indexOf(dataLines[index].lon);
+                sendData.data[latIndex][lonIndex] = dataLines[index].temp;
+
+            }
+
+            console.log(sendData);
 
 
         } catch (e) {
@@ -50,4 +112,3 @@ http.get(util.format(formatUrl, '0:1:99', 39, 143), function (res) {
     });
 });
 
-//http.get('http://nodejs.org/dist/index.json', (res) => {
